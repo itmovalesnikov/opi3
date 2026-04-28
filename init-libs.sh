0913e43bc1fcf07ce359c73bfcbe58dab0f05796
Vladimir Lesnikov <v.a.lesnikov@vk.com>
2026-04-28 18:05:40 +0300

    Update build system to use Ant with enhanced features
    - Refactor build.xml to use build.properties for configuration
    - Add checksum calculation (MD5 and SHA-1) for configuration files
    - Generate and embed Javadoc in the WAR file
    - Separate library dependencies into compile and package scopes
    - Update init-libs.sh to separate compile and runtime dependencies
    - Modify sync.sh to include Ant build alternative

--- init-libs.sh
+++ init-libs.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mvn dependency:copy-dependencies -DoutputDirectory=lib
+set -e
+rm -rf lib/
+mvn dependency:copy-dependencies -DoutputDirectory=lib/compile
+mvn dependency:copy-dependencies -DoutputDirectory=lib/package -DincludeScope=runtime
